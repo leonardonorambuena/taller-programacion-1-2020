6b6a67e8018d4ed60fd651646688d6d6edaaded9
unknown <leonardo.norambuenam@gmail.com>
2020-06-17 20:06:12 -0400

    guía desarrollada

--- Ejercicio01/Program.cs
+++ Ejercicio01/Program.cs
@@ -6,6 +6,132 @@ namespace Ejercicio01
     {
         static void Main(string[] args)
         {
+            int op = 0;
+            Console.WriteLine("----------------------------------------------");
+            Console.WriteLine("Bienvenido! por favor seleccione una opción");
+            Console.WriteLine("1- Ejercicio 01 | 2- Ejercicio 02 | 3- Ejercicio 03 | 4- Ejercicio 04 | 5- 100 primos");
+            Console.WriteLine("----------------------------------------------");
+            try 
+            {
+                op = int.Parse(Console.ReadLine());
+            } catch(Exception e)
+            {
+                Console.WriteLine("Por favor ingrese solamente números");
+                op = 0;
+            }
+            
+            
+
+            switch(op)
+            {
+                case 1: 
+                    eje01();
+                    break;
+                case 2:
+                    eje02();
+                    break;
+                case 3:
+                    eje03();
+                    break;
+                case 4:
+                    eje04();
+                    break;
+                case 5:
+                    eje05();
+                    break;
+            }
+
+        }
+
+        static void eje05()
+        {
+            Console.Clear();
+            int contador = 1;
+            int contadorPrimo = 0;
+
+            while (contadorPrimo <= 100)
+            {
+                contador++;
+                if (primo(contador) == true) {
+                    contadorPrimo++;
+                    Console.WriteLine(contador);
+                }
+            }
+        }
+
+        static void eje04()
+        {
+            Console.Clear();
+            Console.WriteLine("----------------------------------------------");
+            Console.WriteLine("Ejercicio 4");
+            Console.WriteLine("----------------------------------------------");
+            Console.WriteLine("Ingrese un número primo");
+            int numero = int.Parse(Console.ReadLine());
+            Console.WriteLine(primo(numero));
+
+        }
+
+        static bool primo(int numero)
+        {
+            if (numero <= 1)
+            {
+                return false;
+            }
+
+            for(int i = 2; i < numero; i++)
+            {
+                if (numero % i == 0)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+            
+        }
+
+        static void eje03()
+        {
+            Console.Clear();
+            Console.WriteLine("----------------------------------------------");
+            Console.WriteLine("Ejercicio 3");
+            Console.WriteLine("----------------------------------------------");
+
+            try
+            {
+                Console.WriteLine("Ingrese un número");
+                int numero = int.Parse(Console.ReadLine());
+                if (numero % 2 == 0) {
+                    Console.WriteLine(Math.Pow(numero, 3));
+                } 
+                else 
+                {
+                    Console.WriteLine(Math.Pow(numero, 2));
+                }
+            }
+            catch(Exception e) 
+            {
+                Console.WriteLine("Por favor escriba solo números");
+            }
+        }
+
+        static void eje02()
+        {
+            Console.Clear();
+            Console.WriteLine("----------------------------------------------");
+            Console.WriteLine("Cálculo del IMC");
+            Console.WriteLine("----------------------------------------------");
+
+            Console.WriteLine("Ingrese su peso en KG");
+            int masa = int.Parse(Console.ReadLine());
+            Console.WriteLine("Ingrese su estatura en MT");
+            float estatura = float.Parse(Console.ReadLine());
+            var imc = Math.Round((masa / (Math.Pow(estatura, 2))) * 10000, 2);
+            Console.WriteLine($"Su IMC es {imc} %");
+        }
+
+        static void eje01()
+        {
             float distancia = 0, km = 0;
             Console.Clear();
             while (km <= 0)
@@ -34,7 +160,6 @@ namespace Ejercicio01
             }
 
             Console.WriteLine(getResult(km, distancia));
-
         }
 
         static float showMessage()
